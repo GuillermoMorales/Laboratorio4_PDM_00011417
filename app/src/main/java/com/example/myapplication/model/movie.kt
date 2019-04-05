da48242d59431e09e8c6ff89a268b9e6994e1003
GuillermoMorales <00011417@uca.edu.sv>
2019-04-05 07:56:28 -0600

    Inicio

---
+++ app/src/main/java/com/example/myapplication/model/movie.kt
@@ -0,0 +1,63 @@
+package com.example.myapplication.model
+import android.os.Parcel
+import android.os.Parcelable
+
+class movie
+    (
+    val Title:String="N*/A",
+    val Year:String="N*/A",
+    val Released:String="N*/A",
+    val Runtime:String="N*/A",
+    val Genre:String="N*/A",
+    val Director:String="N*/A",
+    val Actors:String="N*/A",
+    val Plot:String="N*/A",
+    val Language:String="N*/A",
+    val imdbRating:String="N*/A",
+    val Poster:String="N*/A"
+    ) : Parcelable{
+    constructor(parcel: Parcel) : this(
+        Title = parcel.readString(),
+        Year=parcel.readString(),
+        Released=parcel.readString(),
+        Runtime=parcel.readString(),
+        Genre=parcel.readString(),
+        Director=parcel.readString(),
+        Actors=parcel.readString(),
+        Plot=parcel.readString(),
+        Language=parcel.readString(),
+        imdbRating=parcel.readString(),
+        Poster=parcel.readString()
+    ) {
+    }
+
+
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(Title)
+        parcel.writeString(Year)
+        parcel.writeString(Released)
+        parcel.writeString(Runtime)
+        parcel.writeString(Genre)
+        parcel.writeString(Director)
+        parcel.writeString(Actors)
+        parcel.writeString(Plot)
+        parcel.writeString(Language)
+        parcel.writeString(imdbRating)
+        parcel.writeString(Poster)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<movie> {
+        override fun createFromParcel(parcel: Parcel): movie {
+            return movie(parcel)
+        }
+
+        override fun newArray(size: Int): Array<movie?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/utils/NetworkUtils.kt
@@ -0,0 +1,48 @@
+package com.example.myapplication.utils
+
+import android.net.Uri
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.*
+
+class NetworkUtils{
+
+    val MOVIES_API_URL = "http://www.omdbapi.com/"
+    val TOKEN_API="91dc807a"
+
+    fun buildSearchUrl(movieName:String):URL
+    {
+        val builtUri = Uri.parse(MOVIES_API_URL).buildUpon().appendQueryParameter("apikey",TOKEN_API).appendQueryParameter("t",movieName).build()
+        return try {
+            URL(builtUri.toString())
+        }catch (e:MalformedURLException){
+            URL("")
+        }
+    }
+    @Throws(IOException::class)
+    fun getResponseFromHttpUrl(url:URL):String
+    {
+        val urlConnection = url.openConnection()as HttpURLConnection//Es como poner la barra del navegador y dar enter
+        try {
+            val `in` = urlConnection.inputStream
+
+            val scanner = Scanner(`in`)
+            scanner.useDelimiter("\\A")
+
+            val hasInput = scanner.hasNext()
+            return if(hasInput)
+            {
+                scanner.next()
+            }else
+            {
+
+            }
+
+        }finally {
+            url
+        }
+    }
+
+}
